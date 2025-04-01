86143b2689421c4e4035abd28fe81cce98c2f923
Gayathri Mahadevan <gayat_19@yahoo.com>
2025-04-01 15:11:52 +0530

    Get and Eager loding

---
+++ Day10/DatabaseFirstApprochApp/DatabaseFirstApprochApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatabaseFirstApprochApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
